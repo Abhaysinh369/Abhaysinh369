f2f0b3afd0d9f403d8f6f80ea7e5545b1bad3e15
Abhaysinh Solanki <asolanki4050@conestogc.on.ca>
2022-02-09 01:22:40 -0500

    commpleted console application

---
+++ Assignment_2/Conversion.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+    public class Conversion
+    {
+        bool validinput = false;
+        float decinput;
+        string input;
+        float temp;
+        public bool shouldRepeat = true;
+        public bool validInput = false;
+        public Conversion()
+        {
+            Program PR = new Program();
+            PR.Choice = 1;
+        }
+        public Conversion(string userinput)
+        {
+            getContinue();
+        }
+        public bool getContinue()
+        {
+            do
+            {
+                Console.Write("Do you want to continue? yes or no: ");
+                
+                switch (Console.ReadLine())
+                {
+                    case "no":
+                        shouldRepeat = false;
+                        validInput = true;
+                        break;
+
+                    case "yes":
+                        shouldRepeat = true;
+                        validInput = true;
+                        break;
+                }
+            }
+            while (!validInput);
+            return shouldRepeat;
+        }
+        public double ConvertCelciusToFahrenheit()
+        {
+            
+            Console.Write("Enter the temperature in Celcius so I can covnert it to Fahrenheit for you: ");
+            do
+            {
+                input = Console.ReadLine();
+                decinput = float.Parse(input);
+               
+                if (decinput >= 0)
+                {
+                    temp = (float)decinput * (float)1.8 + (float)32;
+                    Console.Write("The temperature from Celcius to Farenheit is: {0}", temp);
+                    Console.WriteLine();
+                    shouldRepeat = getContinue();
+                    validinput = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Number");
+                    validinput = false;
+                }
+            } while (validinput != true);
+            return temp;
+
+
+        }
+
+        public double ConvertFahrenheitToCelsius()
+        {
+            Console.Write("Enter the temperature in Fahrenheit so I can convert it to Celsius for you: ");
+
+            do
+            {
+                input = Console.ReadLine();
+                decinput = float.Parse(input);
+
+                if (decinput >= 0)
+                {
+                    temp = ((float)decinput - (float)32) / (float)1.8;
+
+                    Console.Write("The temperature from Farenheit to Celcius is: {0}", temp);
+                    Console.WriteLine();
+                    shouldRepeat = getContinue();
+                    validinput = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Number");
+                    validinput = false;
+                }
+            } while (validinput != true);
+            return temp;
+            
+        }
+        public double ConvertCelciusToKelvin()
+        {
+            Console.Write("Enter the temperature in Celcius so I can convert it to Kelvin for you: ");
+
+            do
+            {
+                input = Console.ReadLine();
+                decinput = float.Parse(input);
+
+                if (decinput >= 0)
+                {
+                    temp = ((float)decinput - (float)273.15);
+
+                    Console.Write("The temperature from Celcius to Kelvin is: {0}", temp);
+                    Console.WriteLine();
+                    shouldRepeat = getContinue();
+                    validinput = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Number");
+                    validinput = false;
+                }
+            } while (validinput != true);
+            return temp;
+            
+        }
+        public double ConvertKelvinToFahrenheit()
+        {
+            Console.Write("Enter the temperature in Kelvin so I can convert it to Fahrenheit for you: ");
+
+            do
+            {
+                input = Console.ReadLine();
+                decinput = float.Parse(input);
+
+                if (decinput >= 0)
+                {
+                    temp = ((float)decinput - (float)273.15) * (float)1.8 + (float)32;
+
+                    Console.Write("The temperature from Kelvin to Fahrenheit is: {0}", temp);
+                    Console.WriteLine();
+                    shouldRepeat = getContinue();
+                    validinput = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Number");
+                    validinput = false;
+                }
+            } while (validinput != true);
+            return temp;
+           
+        }
+        public double ConvertKelvinToCelsius()
+        {
+            Console.Write("Enter the temperature in Kelvin so I can convert it to Celcius for you: ");
+
+            do
+            {
+                input = Console.ReadLine();
+                decinput = float.Parse(input);
+
+                if (decinput >= 0)
+                {
+                    temp = ((float)decinput - (float)273.15);
+
+                    Console.Write("The temperature from Kelvin to Celcius is: {0}", temp);
+                    Console.WriteLine();
+                    shouldRepeat = getContinue();
+                    validinput = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Number");
+                    validinput = false;
+                }
+            } while (validinput != true);
+            return temp;
+
+            
+        }
+        public double ConvertFahrenheitToKelvin()
+        {
+            Console.Write("Enter the temperature in Fahrenheit so I can convert it to Kelvin for you: ");
+
+            do
+            {
+                input = Console.ReadLine();
+                decinput = float.Parse(input);
+
+                if (decinput >= 0)
+                {
+                    temp = ((float)decinput * ((float)5 / (float)9)) + (float)459.67;
+
+                    Console.Write("The temperature from Farenheit to Kelvin is: {0}", temp);
+                    Console.WriteLine();
+                    shouldRepeat = getContinue();
+                    validinput = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Number");
+                    validinput = false;
+                }
+            } while (validinput != true);
+            return temp;
+        }
+
+    }
+}
---
+++ Assignment_2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        public  int Choice;
+        static void Main(string[] args)
+        {
+            
+            String sInput1;
+            
+            Conversion cs = new Conversion();
+            Conversion cs1 = new Conversion("Hello");
+            Program PR = new Program();
+            do
+            {
+                Console.WriteLine("This program converts temperatures between Celsius and Fahrenheit.\n");
+                Console.WriteLine("Which do you want to convert:");
+                Console.WriteLine("1. Celsius to Fahrenheit");
+                Console.WriteLine("2. Celsius to Kelvin\n");
+                Console.WriteLine("3. Kelvin to Fahrenheit\n");
+                Console.WriteLine("4. Kelvin to Celsius\n");
+                Console.WriteLine("5. Fahrenheit to Kelvin\n");
+                Console.WriteLine("6. Fahrenheit to Celsius\n");
+                Console.WriteLine("7. Exit\n");
+
+                Console.Write("Enter 1 or 2 for your choice: ");
+                sInput1 = Console.ReadLine();
+                PR.Choice = Int32.Parse(sInput1);
+
+                switch (PR.Choice)
+                {
+                    case 1:
+                        cs.ConvertCelciusToFahrenheit();
+                        break;
+
+                    case 2:
+                        cs.ConvertCelciusToKelvin();
+                        break;
+                    case 3:
+                        cs.ConvertKelvinToFahrenheit();
+                        break;
+                    case 4:
+                        cs.ConvertKelvinToCelsius();
+                        break;
+                    case 5:
+                        cs.ConvertFahrenheitToKelvin();
+                        break;
+                    case 6:
+                        cs.ConvertFahrenheitToCelsius();
+                        break;
+                    case 7:
+                        cs.shouldRepeat = false;
+                        cs.validInput = true;
+                        break;
+
+                }
+            } while (cs.shouldRepeat);
+            Console.WriteLine("Goodbye!!!");
+            
+        }
+    }
+}
